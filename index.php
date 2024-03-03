6e4dd9324999b51bb395ef0b96d801ffe7bf28e8
Sutrisno <masutrisn021@gmail.com>
2024-03-03 14:04:37 +0700

    feat:making routing use switch case

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
 <?php 
+    // if(isset($_GET['url'])) {
+        $rawUrl = $_GET['url'];
+        echo $rawUrl;
+    //     $path = "view/".$rawUrl.".php";
+    //     // echo $path;
+    //     if(file_exists($path)) {
+    //         include $path;
+    //     }else{
+    //         echo "The $rawUrl is not founded";
+    //     }
+    // }
+
+    // routing using switch case, ussualy is used by simple application
+
     if(isset($_GET['url'])) {
         $rawUrl = $_GET['url'];
-        // echo $rawUrl;
         $path = "view/".$rawUrl.".php";
-        // echo $path;
-        if(file_exists($path)) {
-            include $path;
-        }else{
-            echo "The $rawUrl is not founded";
+
+        switch($rawUrl) {
+            case "":
+                include ("view/home.php");
+                break;
+            case "home":
+                $data = ["title"=>"H O M E P A G E"];
+                include $path;
+                break;
+            case "about":
+                $data = ["title"=>"A B O U T P A G E"];
+                include $path;
+                break;
+            default:
+                echo "The page is not founded";
         }
     }
 ?>
--- view/about.php
+++ view/about.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A B O U T</title>
+    <title><?=$data["title"];?></title>
 </head>
 <body>
     <h1><?=strtoupper($_GET['url']); ?></h1>
